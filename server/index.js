b6922041e98ab3e7fec07d3e5dad9177beab6e9a
EvgeniGamanski <evgeny636@abv.bg>
2025-01-22 15:41:18 +0200

    Commit to main

--- server/index.js
+++ server/index.js
@@ -9,5 +9,11 @@ app.use(express.json())
 app.use(express.static('public'))
 
 /* MONGOOSE SETUP */
-const PORT = 3001
-mongoose.connect()
+const PORT = 3001;
+mongoose.connect(process.env.MONGO_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+})
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`)) 
+    }).catch((err) => console.log(`${err} did not connect`)) 
