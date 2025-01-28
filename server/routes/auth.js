fd1e14d8d11173639ed4c2e329c30a731dd8f7dd
EvgeniGamanski <evgeny636@abv.bg>
2025-01-28 19:47:31 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,13 +5,31 @@ const multer = require("multer")
 
 const User = require("../models/User")
 
+/* Configuration Multer for File Upload */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public/uploads/") // Store upload files in the 'uploads' folder
+    },
+    filename: function ( req,file,cb ){
+        cb(null, file.originalname) // Use the original file name
+    }
+})
+
+const upload = multer({ storage })
+
 /* register */
 router.post("/register", upload.single('profileImage'), async (req, res) => {
     try{
         /* take all info from form */
         const { firstName, lastName, email, password } = req.body
-    }
-    catch (err) {
+
+        /* the uploaded file is available as req.file */ 
+        const profileImage = req.file
+
+        if(!profileImage){
+            return res.status(400).send("No file uploaded")
+        }
+    } catch (err) {
 
     }
 })
