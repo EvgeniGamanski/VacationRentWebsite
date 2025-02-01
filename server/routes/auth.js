635c3b18b9909aa05e95d66f1468db27eddbe15e
EvgeniGamanski <evgeny636@abv.bg>
2025-02-01 20:14:57 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -54,7 +54,10 @@ router.post("/register", upload.single('profileImage'), async (req, res) => {
 
         /* Save the new User */
         await newUser.save()
-    } catch (err) {
 
+        res.status(200).json({ message: "User registered successfully!", user: newUser })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Registration failed!", error: err.message })
     }
 })
