158e7102964e8da337d3476cfd4be10f2701684f
EvgeniGamanski <evgeny636@abv.bg>
2025-02-22 14:26:24 +0200

    Initial Commit

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../styles/Login.scss"
-import { setLogin } from '../redux/state';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { setLogin } from "../redux/state";
+// import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -10,60 +11,62 @@ const LoginPage = () => {
 
   const dispatch = useDispatch()
 
-  const navigate = useNavigate()
+  const navigate = useNavigate() 
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     try {
-      const response = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ email, password })
-    })
+      const response = await fetch ("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      })
 
-    /* Get data after fetching */
-    const loggedIn = await response.json()
-    
-    if(loggedIn){
-      dispatch(
-        setLogin({
-          user: loggedIn.user,
-          token: loggedIn.token
-        })
-      )
-      navigate("/")
-    }
+      /* Get data after fetching */
+      const loggedIn = await response.json()
 
-    } catch (err){
+      if (loggedIn) {
+        dispatch (
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate("/")
+      }
 
+    } catch (err) {
+      console.log("Login failed", err.message)
     }
   }
+
   return (
-    <div className='login'>
-      <div className='login_content'>
-        <form className='login_content_form'>
-          <input 
-            type="email" 
-            placeholder='Email' 
-            value={email} 
-            onChange={(e) => e.target.value} 
-            required 
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
-          <input 
-            type="password" 
-            placeholder='Password' 
-            value={password} onChange={(e) => e.target.value} 
-            required 
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
           />
-          <button type='submit'>LOG IN</button>
+          <button type="submit">LOG IN</button>
         </form>
-        <a href="/register">Don't Have An Account? Sign In Here.</a>
+        <a href="/register">Don't have an account? Sign In Here</a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
