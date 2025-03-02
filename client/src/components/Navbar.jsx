401012e46a552df5b88882698147031c60358579
EvgeniGamanski <evgeny636@abv.bg>
2025-03-02 12:44:39 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,44 @@
 import { IconButton } from "@mui/material"
-import { Search } from "@mui/icons-material"
+import { Search, Person, Menu } from "@mui/icons-material"
 import variables from "../styles/variables.scss"
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        <a href='/'> 
-         <img src="/assets/logo.png" alt="logo" />
-        </a>      
+      <a href='/'>
+        <img src="/assets/logo.png" alt="logo" />
+      </a>
 
-        <div className='navbar_search'>
-            <input type="text" placeholder='Search ...' />
-            <IconButton>
-              <Search sx={{ color: variables.pinkred }}/>
-            </IconButton>
-        </div>
+      <div className='navbar_search'>
+        <input type="text" placeholder='Search ...' />
+        <IconButton>
+          <Search sx={{ color: variables.pinkred }} />
+        </IconButton>
+      </div>
 
-        <div className="navbar_right"></div>
+      <div className="navbar_right">
+        {user ? (
+          <a href="/create-listing">Become A Host</a>
+        ) : (
+          <a href="/login">Become A Host</a>
+        )}
+
+        <button className="navbar_right_account">
+          <Menu sx={{ color: variables.darkgrey }} />
+          {!user ?
+            <Person sx={{ color: variables.darkgrey }} />
+            : (
+              <img 
+                  src={`http://localhost:3001/${user.profileImagePath.replace(
+                  "public", 
+                  ""
+                )}`} 
+                alt="profile photo" 
+                style={{ objectFit: "cover", borderRadius: "50%" }}
+              />
+            )}
+        </button>
+      </div>
     </div>
   )
 }
