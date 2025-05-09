3afc97c13da6c48ce4bbcd5982d822a3d88bae0e
EvgeniGamanski <evgeny636@abv.bg>
2025-05-09 18:29:28 +0300

    Update Listing.js

--- server/models/Listing.js
+++ server/models/Listing.js
@@ -58,6 +58,26 @@ const ListingSchema = new mongoose.Schema(
         title: {
             type: String,
             required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        highlight: {
+            type: String,
+            required: true
+        },
+        highlightDesc: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
         }
-    }
+    },
+    { timestamps: true }
 )
+
+const Listing = mongoose.model("Listing", ListingSchema )
+module.exports = Listing
